d99a77884135f1fafcb41bc9e7ba01476a98bacb
JayantiTA <jayjayanti18@gmail.com>
2023-07-21 16:35:54 +0700

    refactor: unused function

--- utils/converter.go
+++ utils/converter.go
@@ -1,17 +1,8 @@
 package utils
 
 import (
-	"strconv"
 	"time"
 )
-
-func StringToInt64(text string) (int64, error) {
-	num, err := strconv.ParseInt(text, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-	return num, nil
-}
 
 func StringToDate(text string) (time.Time, error) {
 	layout := "2006-01-02"
@@ -20,12 +11,4 @@ func StringToDate(text string) (time.Time, error) {
 		return time.Time{}, err
 	}
 	return date, nil
-}
-
-func UnixToTime(unix string) (time.Time, error) {
-	unixInt, err := StringToInt64(unix)
-	if err != nil {
-		return time.Time{}, err
-	}
-	return time.Unix(unixInt, 0), nil
 }
